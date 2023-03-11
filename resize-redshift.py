7699d5d20f6f794f8718785e2b4a49ec2813f9da
John Dauphine <jdauphine@gmail.com>
2023-03-11 17:44:22 -0600

    added helper functions

--- resize-redshift.py
+++ resize-redshift.py
@@ -1,25 +1,52 @@
-import boto3
+import json
 import logging
 
-# Define the Redshift cluster identifier and the new node type and count
-cluster_identifier = 'redshift-cluster-1'
-new_node_type = 'dc2.large'
-new_node_count = 2
+import boto3
 
-#Logging
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+def get_ssm_secret(parameter_name):
+    ssm = boto3.client("ssm", region_name='us-east-1')
+    secret =  ssm.get_parameter( Name=parameter_name, WithDecryption=True  )
+    value = secret.get('Parameter').get('Value')
+    del ssm
+    return value
 
-# Create the Redshift client
-redshift = boto3.client('redshift',region_name='us-east-1')
+def format_response(status_code, message ):
+    
+    header = {"Content-Type": "application/json", "Accept": "application/json"}
+    format_output = {"statusCode": int(status_code), "headers": header, "body": json.dumps(message)}
+    
+    return format_output
 
-# Call the modify_cluster API with the new node type and count
-response = redshift.modify_cluster(
-    ClusterIdentifier=cluster_identifier,
-    NodeType=new_node_type,
-    NumberOfNodes=new_node_count
-)
 
-# Print the API response
-print(response)
+def main():
+    # Define the Redshift cluster identifier and the new node type and count
+    cluster_identifier = 'redshift-cluster-1'
+    new_node_type = 'dc2.large'
+    new_node_count = 4
 
+    #Logging
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    try:
+        
+        # Create the Redshift client
+        redshift = boto3.client('redshift',region_name='us-east-1')
+
+        # Call the modify_cluster API with the new node type and count
+        response = redshift.modify_cluster(
+            ClusterIdentifier=cluster_identifier,
+            NodeType=new_node_type,
+            NumberOfNodes=new_node_count
+        )
+
+        # Print the API response
+        return format_response(200,response)
+    except Exception as exc:
+        logger.error('Unhandled exception', exc_info=True )
+        return format_response(400,str(exc))
+
+
+
+if __name__ == "__main__":
+    main()
+    
