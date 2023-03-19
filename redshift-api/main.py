dab141d3b1a6f7897e585150da8e0ac21342c1a6
John Dauphine <jdauphine@gmail.com>
2023-03-18 22:00:17 -0500

    Added resume redshift

--- redshift-api/main.py
+++ redshift-api/main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI,HTTPException
 import logging
 from datetime import datetime
-from  redshift import pause_cluster
+from  redshift import pause_cluster,resume_cluster,resize_cluster
 
 
 app = FastAPI()
@@ -16,6 +16,10 @@ async def index():
 @app.get("/redshift/api/v1/{cluster_identifier}/pause")
 async def pause_redshift_cluster(cluster_identifier):
     return pause_cluster.pause(cluster_identifier=cluster_identifier)
+
+@app.get("/redshift/api/v1/{cluster_identifier}/resume")
+async def resume_redshift_cluster(cluster_identifier):
+    return resume_cluster.resume(cluster_identifier=cluster_identifier)
 
 
 @app.get("/datetime")
