fe49a9e5af5437f649808c1dc84955a4637da1ec
John Dauphine <jdauphine@gmail.com>
2023-03-18 22:20:18 -0500

    Reorganized functions into one module

--- redshift-api/main.py
+++ redshift-api/main.py
@@ -2,12 +2,10 @@ import uvicorn
 from fastapi import FastAPI,HTTPException
 import logging
 from datetime import datetime
-from  redshift import pause_cluster,resume_cluster,resize_cluster
+from redshift.cluster_operations import pause,resize,resume
 
 
 app = FastAPI()
-
-
 
 @app.get("/")
 async def index():
@@ -15,11 +13,11 @@ async def index():
 
 @app.get("/redshift/api/v1/{cluster_identifier}/pause")
 async def pause_redshift_cluster(cluster_identifier):
-    return pause_cluster.pause(cluster_identifier=cluster_identifier)
+    return pause(cluster_identifier=cluster_identifier)
 
 @app.get("/redshift/api/v1/{cluster_identifier}/resume")
 async def resume_redshift_cluster(cluster_identifier):
-    return resume_cluster.resume(cluster_identifier=cluster_identifier)
+    return resume.resume(cluster_identifier=cluster_identifier)
 
 
 @app.get("/datetime")
@@ -34,7 +32,6 @@ async def index(name):
 
 if __name__=="__main__":
     try:
-
         uvicorn.run("main:app",host="0.0.0.0", port=8000,reload=True)
     except Exception as exc:
         print(exc)
