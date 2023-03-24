c516cf9021699c3f3a42dec50f60dbf5688d6e9b
John Dauphine <jdauphine@gmail.com>
2023-03-23 20:00:29 -0500

    added get_age

--- class_playground/classes.py
+++ class_playground/classes.py
@@ -2,21 +2,21 @@
 from datetime import date,datetime
 
 class Person:
-    def __init__(self,first_name,last_name,birth_date):
+    def __init__(self,first_name:str,last_name:str,birth_date:date):
         self.first_name = first_name
         self.last_name = last_name
         self.birth_date = birth_date
-        self.age = self.calculate_age(datetime.strptime(birth_date,'%Y-%m-%d' ))
+        self.age = self.get_age()
 
     def __repr__(self) -> str:
         return f"Person('{self.first_name}','{self.last_name}','{self.birth_date}')"
 
     def __str__(self) -> str:
-        return f"Name:{self.first_name} {self.last_name} DOB:{self.birth_date}"
+        return f"Name:{self.first_name} {self.last_name} Age:{self.age}"
     
-    def calculate_age(self,born):
+    def get_age(self):
         today = date.today()
-        return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+        return today.year - self.birth_date.year - ((today.month, today.day) < (self.birth_date.month, self.birth_date.day))
 
 class Employee(Person):
     def __init__(self, first_name, last_name, birth_date,salary):
@@ -27,17 +27,17 @@ class Employee(Person):
         return f"Employee('{self.first_name}','{self.last_name}','{self.birth_date}',{self.salary})"
 
     def __str__(self) -> str:
-        return f"Name:{self.first_name} {self.last_name} DOB:{self.birth_date} Salary:{self.salary}"
+        return f"Name:{self.first_name} {self.last_name} Age:{self.age} Salary:{self.salary}"
 
 
 
 
 
-person1 = Person('John','Dauphine','1969-11-25')
+person1 = Person('John','Dauphine',date(1969,11,25))
 
-employee1 = Employee('Connie','Williams','1969-08-14',50000)
+person2 = Person('Connie','Williams',date(1969,8,14))
 
-print(person1)
-print(person1.age)
+people = [person1,person2]
 
-print(employee1)
+for p in people:
+    print(p.first_name)
