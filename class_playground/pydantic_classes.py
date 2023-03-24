2460752a10f274331f11c22c8c39528f2b269e4e
John Dauphine <jdauphine@gmail.com>
2023-03-23 20:46:34 -0500

    added validators to class

--- class_playground/pydantic_classes.py
+++ class_playground/pydantic_classes.py
@@ -1,7 +1,7 @@
 
 from datetime import date,datetime
-
-from pydantic import BaseModel
+from typing import List
+from pydantic import BaseModel, ValidationError,validator
 from enum import Enum
 
 
@@ -14,26 +14,40 @@ class Person(BaseModel):
     last_name:str
     birth_date:date
     gender:Gender
-  
+    
+    @validator('first_name')
+    def check_first_name(cls,value:str):
+        if value.isdigit():
+            raise ValueError("First name must be a character value")
+        return value
+
+    @validator('last_name')
+    def check_last_name(cls,value:str):
+        if value.isdigit():
+            raise ValueError("Last name must be a character value")
+        return value
+
+
 
 class Employee(Person):
-    first_name:str
-    last_name:str
-    birth_date:date
     salary:int
-    gender:Gender
 
 
 
 
 
-person1 = Person( first_name='John',last_name='Dauphine',birth_date=date(1969,11,25),gender=Gender.male)
 
-person2 = Person(first_name='Connie',last_name='Williams',birth_date=date(1969,8,14),gender=Gender.female)
+person1 = Person( first_name='John',last_name='Dauphine',
+                 birth_date=date(1969,11,25),gender=Gender.male)
 
-people = [person1,person2]
+employee1 = Employee(first_name='Connie',last_name='666',
+                     birth_date=date(1969,8,14),gender=Gender.female,salary=50000)
+
+
+
+people:List[Person] = [person1,employee1]
 
 for p in people:
     print(p.first_name)
 
-print(person1)
+print(employee1)
