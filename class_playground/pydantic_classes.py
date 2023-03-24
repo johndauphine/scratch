041d7f4356d10156109e4bde56b3c82a165dd87e
John Dauphine <jdauphine@gmail.com>
2023-03-23 21:03:54 -0500

    added birth_date validator

--- class_playground/pydantic_classes.py
+++ class_playground/pydantic_classes.py
@@ -15,6 +15,11 @@ class Person(BaseModel):
     birth_date:date
     gender:Gender
     
+    def get_age(cls,birth_date):
+        today = date.today()
+        return today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+
+
     @validator('first_name')
     def check_first_name(cls,value:str):
         if value.isdigit():
@@ -27,20 +32,21 @@ class Person(BaseModel):
             raise ValueError("Last name must be a character value")
         return value
 
+    @validator('birth_date')
+    def check_birth_date(cls,value:date):
+        if Person.get_age(cls,value) < 18:
+            raise ValueError("Birth date must be at least 18 years ago")
+        return value
 
 
 class Employee(Person):
     salary:int
 
 
-
-
-
-
 person1 = Person( first_name='John',last_name='Dauphine',
                  birth_date=date(1969,11,25),gender=Gender.male)
 
-employee1 = Employee(first_name='Connie',last_name='666',
+employee1 = Employee(first_name='Connie',last_name='Williams',
                      birth_date=date(1969,8,14),gender=Gender.female,salary=50000)
 
 
