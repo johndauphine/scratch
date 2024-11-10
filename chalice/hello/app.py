72e02a29afd36854b54f5feb1a21a4dcd493cb8e
John Dauphine <jdauphine@gmail.com>
2024-11-10 10:11:22 -0600

    uncommented function

--- chalice/hello/app.py
+++ chalice/hello/app.py
@@ -1,5 +1,9 @@
 from chalice import Chalice
 
+import pandas
+import requests
+import marshmallow
+
 app = Chalice(app_name='hello')
 
 
@@ -18,12 +22,12 @@ def hello_name(name):
     # '/hello/james' -> {"hello": "james"}
     return {'message': f'Hello {name}'}
 #
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
+@app.route('/users', methods=['POST'])
+def create_user():
+    # This is the JSON body the user sent in their POST request.
+    user_as_json = app.current_request.json_body
+    # We'll echo the json body back to the user in a 'user' key.
+    return {'user': user_as_json}
 #
 # See the README documentation for more examples.
 #
